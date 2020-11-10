9b22f7f82143a7c78e97d14209d3f9df625ebba1
Ronnie Bar-Kochba <ronnie.barkochba@gmail.com>
2020-11-10 10:40:29 -0500

    Added example project.

---
+++ Cyclex/Cyclex.Example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cyclex.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
