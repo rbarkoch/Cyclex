c90a1a9392cd55d728ad735c76d2477b2c6ec92e
Ronnie Bar-Kochba <ronnie.barkochba@gmail.com>
2020-11-10 09:36:17 -0500

    Added base interface for cyclic executives.

---
+++ Cyclex/Cyclex/CyclicExecutive.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cyclex
+{
+    public class CyclicExecutive : ICyclicExecutive
+    {
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Cyclex/Cyclex/ICyclicExecutive.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cyclex
+{
+    public interface ICyclicExecutive
+    {
+        public void Start();
+        public void Stop();
+    }
+}
